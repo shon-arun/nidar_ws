c92b8c0307f02a5fddf9f10d54877cff0a9d3e35
Shon Arun <shonarun3785@gmail.com>
2026-03-11 17:10:16 +0530

    feat: initialize ROS2 package architecture
    Generated boilerplate packages for core control, perception, mission autonomy, custom interfaces, and URDF descriptions.

---
+++ src/opsg_mission/setup.py
@@ -0,0 +1,29 @@
+from setuptools import find_packages, setup
+
+package_name = 'opsg_mission'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='Shon Arun',
+    maintainer_email='shonarun3785@gmail.com',
+    description='High-level mission autonomy, state machines, and multi-drone coordination for OpSG rescue operations.',
+    license='Apache-2.0',
+    extras_require={
+        'test': [
+            'pytest',
+        ],
+    },
+    entry_points={
+        'console_scripts': [
+        ],
+    },
+)
