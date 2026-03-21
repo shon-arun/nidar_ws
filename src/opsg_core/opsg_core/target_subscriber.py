415c7f759317e479d066fd67f41241822e239352
Shom Arun <shonarun3785@gmail.com>
2026-03-21 11:20:38 +0530

    feat(core): upgrade target_node to publish offboard setpoints
    Transformed the target node into a Dynamic Flight Commander. It now subscribes to /target_altitude, converts the data to the NED coordinate system, and publishes live TrajectorySetpoint commands to the PX4 offboard controller.

--- src/opsg_core/opsg_core/target_subscriber.py
+++ src/opsg_core/opsg_core/target_subscriber.py
@@ -2,6 +2,7 @@
 import rclpy
 from rclpy.node import Node
 from std_msgs.msg import Float32
+from px4_msgs.msg import TrajectorySetpoint
 
 class TargetSubscriber(Node):
     def __init__(self):
@@ -13,9 +14,24 @@ class TargetSubscriber(Node):
             self.listener_callback,
             10)
         self.subscription  # prevent unused variable warning
+        self.trajectory_pub = self.create_publisher(TrajectorySetpoint, '/fmu/in/trajectory_setpoint', 10)
+
+        self.get_logger().info('Target Commander Active. Waiting for altitude commands...')
 
     def listener_callback(self, msg):
-        self.get_logger().info(f'Received Target Altitude: {msg.data} m')
+        target_alt = msg.data
+        self.get_logger().info(f'Executing command: Ascending to {target_alt} meters')
+        # self.get_logger().info(f'Received Target Altitude: {msg.data} m')
+
+        # Create the PX4 physical command
+        setpoint_msg = TrajectorySetpoint()
+
+        # Conversion to the NED coordinate system
+        setpoint_msg.position = [0.0, 0.0, -target_alt]
+        setpoint_msg.yaw = 0.0 # Keep the drone facing North
+        setpoint_msg.timestamp = int(self.get_clock().now().nanoseconds / 1000)
+
+        self.trajectory_pub.publish(setpoint_msg)
 
 def main(args=None):
     rclpy.init(args=args)
